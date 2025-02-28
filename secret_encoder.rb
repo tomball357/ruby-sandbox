d66acbc363269f1a27e84eba3b2eb8858e33c7fe
tomball357 <tball@chicagobooth.edu>
2025-02-28 02:24:04 +0000

    hw_learn

---
+++ secret_encoder.rb
@@ -0,0 +1,20 @@
+secrets = [
+  "I have a secret to share",
+  "Is this secure enough for my password?",
+  "we should be more clever",
+  "I need to be more secret",
+  "Don't tell anyone our code"
+]
+
+str = secrets.sample
+pp str.tr("aeiou","12345")
+
+#puts.gsub
+#"a" == 1.to_s
+#"e" == 2.to_s
+#"i" == 3.to_s
+#"o" == 4.to_s
+#"u" == 5.to_s
+
+#pp secrets.sample
+# write your program below
